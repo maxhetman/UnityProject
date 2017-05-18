e4ffa3bde79343188b87d03c13722fc290e5b8ee
Max Hetman <maxxhetman@gmail.com>
2017-05-18 23:37:52 +0300

    rabbit movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform RabitTransform;
+
+    private Vector2 _offset;
+    void Awake()
+    {
+        _offset = new Vector2(transform.position.x - RabitTransform.position.x,
+            transform.position.y - RabitTransform.position.y);
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(
+            RabitTransform.position.x - _offset.x, 
+            RabitTransform.position.y - _offset.y, 
+            transform.position.z);
+    }
+  
+}
---
+++ Assets/Scripts/RabbitController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitController : MonoBehaviour
+{
+
+    public float Speed = 2.0f;
+    private Rigidbody2D _rigidBody;
+    private SpriteRenderer _spriteRenderer;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        float xSpeed = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(xSpeed) > 0)
+        {
+            Vector2 vel = _rigidBody.velocity;
+            vel.x = xSpeed * Speed;
+            _rigidBody.velocity = vel;
+        }
+
+        if (xSpeed < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else if (xSpeed > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+    }
+}
